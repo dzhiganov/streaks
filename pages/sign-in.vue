43599b592be2df25e784f86f4e3d3f273ed55c47
d.zhiganov <delawere2009@yandex.ru>
2025-01-07 21:46:39 +0100

    fix template

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,30 +1,33 @@
 <script setup>
-import { GoogleIcon } from '../assets/icons'
+import { GoogleIcon } from '../assets/icons';
 
 definePageMeta({
-    middleware: ['auth']
-})
+  middleware: ['auth'],
+});
 
 definePageMeta({
   auth: {
     unauthenticatedOnly: true,
-    navigateAuthenticatedTo: '/app'
-  }
-})
+    navigateAuthenticatedTo: '/app',
+  },
+});
 
-const { signIn } = useAuth()
+const { signIn } = useAuth();
 </script>
 <template>
+  <div class="h-screen w-full">
     <div class="card mx-auto w-96 shadow-md flex justify-center flex-col gap-4 mt-16 p-6">
       <div class="flex flex-col gap-8 w-full justify-center items-center">
         <button @click="signIn('google')" class="btn w-full btn-neutral">
-            <GoogleIcon />
-            Sign in with Google
+          <GoogleIcon />
+          Sign in with Google
         </button>
       </div>
       <div class="text-center mt-4">
-        By proceeding, you agree to the <NuxtLink class="link-primary" href="/tos">Terms of Service</NuxtLink> and <NuxtLink class="link-primary" href="/privacy-policy">Privacy Notice</NuxtLink>.
+        By proceeding, you agree to the
+        <NuxtLink class="link-primary" href="/tos">Terms of Service</NuxtLink> and
+        <NuxtLink class="link-primary" href="/privacy-policy">Privacy Notice</NuxtLink>.
       </div>
-
-    </div>  
+    </div>
+  </div>
 </template>
