0e2d003f448191ecebb3f563bc6934b1b72889db
d.zhiganov <delawere2009@yandex.ru>
2025-02-02 17:51:29 +0100

    fix progress colors

--- components/TimeGoal.vue
+++ components/TimeGoal.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { CheckIcon } from '~/assets/icons';
 import { formatTime } from '~/utils/time/formatTime';
 
 const props = defineProps({
@@ -37,14 +36,14 @@ const progress = computed(() => {
 });
 
 const progressColor = computed(() => {
-  if (progress.value >= 100) {
+  if (progress.value > 100) {
     return 'text-orange-400';
-  } else if (progress.value >= 80) {
-    return 'text-green-700';
+  } else if (progress.value === 100) {
+    return 'text-green-600';
   } else if (progress.value >= 50) {
-    return 'text-green-500';
-  } else {
     return 'text-neutral';
+  } else {
+    return 'text-gray-400';
   }
 });
 </script>
@@ -63,17 +62,6 @@ const progressColor = computed(() => {
       <p class="w-full font-medium flex gap-2 w-full truncate font-semibold">
         {{ activityTitle }}
       </p>
-      <div>
-        <div class="text-primary">
-          <span
-            v-if="getProgress(goalMinutes, restMinutes) >= 100"
-            class="flex items-center gap-1 text-success"
-          >
-            <CheckIcon />
-            <span>Done</span>
-          </span>
-        </div>
-      </div>
 
       <div class="flex items-center gap-2 text-sm text-gray-500">
         <span>{{ title }}</span>
