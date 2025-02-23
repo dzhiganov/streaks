94bccb73ea6f3d0d5ddd2799906349d7475b42d3
d.zhiganov <delawere2009@yandex.ru>
2025-02-23 18:47:02 +0100

    update deploy file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'habit-tracker',
+      script: './.output/server/index.mjs',
+      env: {
+        NODE_ENV: 'production',
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        AUTH_ORIGIN: process.env.AUTH_ORIGIN,
+      },
+    },
+  ],
+};
